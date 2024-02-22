7b1f15fb4bf72d97d0fe1e131ae3f59c622129b5
SergoGansta777 <sergo777ser777@gmail.com>
2024-02-22 21:53:44 +0300

    Improved login and cookie security

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -30,9 +30,10 @@ pub async fn login(
     match validate_credentials(credentials, &pool).await {
         Ok(user_id) => {
             tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
+            session.renew();
             session
                 .insert("user_id", user_id)
-                .map_err(|e| login_redirect(LoginError::UnexpectedError(e.into())));
+                .map_err(|e| login_redirect(LoginError::UnexpectedError(e.into())))?;
             Ok(HttpResponse::SeeOther()
                 .insert_header((LOCATION, "/admin/dashboard"))
                 .finish())

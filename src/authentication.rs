3a15d29c0793351cb8e27f234cdfe1527a71fe1f
SergoGansta777 <sergo777ser777@gmail.com>
2024-02-26 10:51:48 +0300

    Added possiblity to change password

--- src/authentication.rs
+++ src/authentication.rs
@@ -1,6 +1,8 @@
-use crate::telemetry::spawn_blockign_with_tracing;
+use crate::telemetry::spawn_blocking_with_tracing;
+use actix_web_flash_messages::FlashMessage;
 use anyhow::Context;
-use argon2::{Argon2, PasswordHash, PasswordVerifier};
+use argon2::password_hash::SaltString;
+use argon2::{Algorithm, Argon2, Params, PasswordHash, PasswordHasher, PasswordVerifier, Version};
 use secrecy::{ExposeSecret, Secret};
 use sqlx::PgPool;
 
@@ -31,7 +33,7 @@ pub async fn validate_credentials(
         expected_password_hash = stored_password_hash;
     }
 
-    spawn_blockign_with_tracing(move || {
+    spawn_blocking_with_tracing(move || {
         verify_password_hash(expected_password_hash, credentials.password)
     })
     .await
@@ -80,6 +82,42 @@ async fn get_stored_credentials(
     .context("Failed to perfomr a query to retriee stored credentials.")?
     .map(|row| (row.user_id, Secret::new(row.password_hash)));
     Ok(row)
+}
+
+#[tracing::instrument(name = "Change pasword", skip(password, pool))]
+pub async fn change_password(
+    user_id: uuid::Uuid,
+    password: Secret<String>,
+    pool: &PgPool,
+) -> Result<(), anyhow::Error> {
+    let password_hash = spawn_blocking_with_tracing(move || compute_password_hash(password))
+        .await?
+        .context("Failed to hash password")?;
+    sqlx::query!(
+        r#"
+       UPDATE users
+       SET password_hash = $1
+       WHERE user_id = $2
+       "#,
+        password_hash.expose_secret(),
+        user_id
+    )
+    .execute(pool)
+    .await
+    .context("Failed to change user's password in the database.")?;
+    Ok(())
+}
+
+fn compute_password_hash(password: Secret<String>) -> Result<Secret<String>, anyhow::Error> {
+    let salt = SaltString::generate(&mut rand::thread_rng());
+    let password_hash = Argon2::new(
+        Algorithm::Argon2id,
+        Version::V0x13,
+        Params::new(15000, 2, 1, None).unwrap(),
+    )
+    .hash_password(password.expose_secret().as_bytes(), &salt)?
+    .to_string();
+    Ok(Secret::new(password_hash))
 }
 
 #[derive(thiserror::Error, Debug)]

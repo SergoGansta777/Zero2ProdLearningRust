cc00aeda2a891c22d41115f085e2c974d0fe530f
SergoGansta777 <sergo777ser777@gmail.com>
2024-02-28 22:48:44 +0300

    feat: update dependencies versions
    - Update actix-codec to version 0.5.2
    - Update actix-http to version 3.6.0
    - Update actix-web to version 4.5.1
    - Update actix-utils to version 3.0.1
    - Add actix-session version 0.9.0
    - Add actix-web-flash-messages version 0.4.2
    - Add actix-web-lab version 0.20.2
    - Add actix-web-lab-derive version 0.20.0
    - Add aead dependency

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,14 @@
-use env_logger::{self, Env};
-use sqlx::PgPool;
-use std::net::TcpListener;
-
-use zero2prod::{configuration::get_configuration, startup::run};
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::Application;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+async fn main() -> anyhow::Result<()> {
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
-    let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+    let application = Application::build(configuration).await?;
+    application.run_until_stopped().await?;
+    Ok(())
 }
---
+++ src/routes/admin/password/get.rs
@@ -0,0 +1,30 @@
+use crate::session_state::TypedSession;
+use crate::utils::{e500, see_other};
+use actix_web::http::header::ContentType;
+use actix_web::HttpResponse;
+use actix_web_flash_messages::IncomingFlashMessages;
+use std::fmt::Write;
+
+pub async fn change_password_form(
+    session: TypedSession,
+    flash_messages: IncomingFlashMessages,
+) -> Result<HttpResponse, actix_web::Error> {
+    if session.get_user_id().map_err(e500)?.is_none() {
+        return Ok(see_other("/login"));
+    }
+    let mut msg_html = String::new();
+    for m in flash_messages.iter() {
+        writeln!(msg_html, "<i><p>{}</i></p>", m.content()).unwrap();
+    }
+
+    Ok(HttpResponse::Ok()
+        .content_type(ContentType::html())
+        .body(format!(
+            r#"<!-- [...] -->
+    <body>
+        {msg_html}
+        <!-- [...] -->
+    </body>
+    </html>"#,
+        )))
+}
